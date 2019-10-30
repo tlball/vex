04fd7a2da179631097725d8fc70259beb436dbc3
Tricia Ball <trish.ball@gmail.com>
2019-10-29 22:34:09 -0500

    Test github integration - update comments
    Updated some comments to test working with the Github desktop client.

--- 2019/367G/include/cube_commands.cpp
+++ 2019/367G/include/cube_commands.cpp
@@ -9,13 +9,15 @@ void grabberButtons() {
   // Right, top button spins grabbers in reverse
   // Stop grabbers when buttons aren't pressed
   if (Controller1.ButtonL1.pressing()) {
+    // Left and Right grabbers run forward when pressing L1
     Left_Grabber.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-    // Right grabber, botton R1
     Right_Grabber.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
   } else if (Controller1.ButtonR1.pressing()) {
+    // Left and Right grabbers run in reverse when pressing R1
     Left_Grabber.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
     Right_Grabber.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
   } else {
+    // Stop motors if neither button pressed
     Left_Grabber.stop();
     Right_Grabber.stop();
   }
@@ -23,13 +25,15 @@ void grabberButtons() {
 
 void armButtons() {
   if (Controller1.ButtonL2.pressing()) {
+    // Left and Right arms move up when pressing L2
     Left_Arm.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-    // Right arm, botton R2
     Right_Arm.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
   } else if (Controller1.ButtonR2.pressing()) {
+    // Left and Right arms move down when pressing R2
     Left_Arm.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
     Right_Arm.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
   } else {
+    // Stop motors if neither button pressed
     Left_Arm.stop();
     Right_Arm.stop();
   }
