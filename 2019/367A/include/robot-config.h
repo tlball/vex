59fdb04729019339ad6b913929a283d8bb083f06
Tricia Ball <trish.ball@gmail.com>
2019-11-07 20:13:49 -0600

    First commit
    Drive and Flipmotor

---
+++ 2019/367A/include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern controller Controller1;
+extern motor FlipMotor;
+extern motor Rightintake;
+extern motor Leftintake;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
