e8e0db9c94571a17365f4bd02e8440c09b490795
Tricia Ball <trish.ball@gmail.com>
2019-11-21 18:01:15 -0600

    Add hold to the arm/grabber motor stops and reduce speed
    Sophia updated - Arms were slipping so needed to add holds, and
    arm/grabbers were going too fast, so slowed them down.

--- 2019/367G/include/cube_commands.cpp
+++ 2019/367G/include/cube_commands.cpp
@@ -10,31 +10,31 @@ void grabberButtons() {
   // Stop grabbers when buttons aren't pressed
   if (Controller1.ButtonL1.pressing()) {
     // Left and Right grabbers run forward when pressing L1
-    Left_Grabber.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-    Right_Grabber.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+    Left_Grabber.spin(vex::directionType::fwd, 25, vex::velocityUnits::pct);
+    Right_Grabber.spin(vex::directionType::fwd, 25, vex::velocityUnits::pct);
   } else if (Controller1.ButtonR1.pressing()) {
     // Left and Right grabbers run in reverse when pressing R1
-    Left_Grabber.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
-    Right_Grabber.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+    Left_Grabber.spin(vex::directionType::rev, 25, vex::velocityUnits::pct);
+    Right_Grabber.spin(vex::directionType::rev, 25, vex::velocityUnits::pct);
   } else {
     // Stop motors if neither button pressed
-    Left_Grabber.stop();
-    Right_Grabber.stop();
+    Left_Grabber.stop(hold);
+    Right_Grabber.stop(hold);
   }
 }
 
 void armButtons() {
   if (Controller1.ButtonL2.pressing()) {
     // Left and Right arms move up when pressing L2
-    Left_Arm.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-    Right_Arm.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+    Left_Arm.spin(vex::directionType::fwd, 25, vex::velocityUnits::pct);
+    Right_Arm.spin(vex::directionType::fwd, 25, vex::velocityUnits::pct);
   } else if (Controller1.ButtonR2.pressing()) {
     // Left and Right arms move down when pressing R2
-    Left_Arm.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
-    Right_Arm.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+    Left_Arm.spin(vex::directionType::rev, 25, vex::velocityUnits::pct);
+    Right_Arm.spin(vex::directionType::rev, 25, vex::velocityUnits::pct);
   } else {
     // Stop motors if neither button pressed
-    Left_Arm.stop();
-    Right_Arm.stop();
+    Left_Arm.stop(hold);
+    Right_Arm.stop(hold);
   }
 }
