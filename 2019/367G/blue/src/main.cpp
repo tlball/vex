d0cb99a484059317624b3916b95bce941db88367
Vex 367G <SolonRobotics+367G@googlegroups.com>
2020-01-28 18:20:06 -0600

    Blue auto - keep grabbers going, increase turn

--- 2019/367G/blue/src/main.cpp
+++ 2019/367G/blue/src/main.cpp
@@ -64,13 +64,13 @@ void pickerupper(void) {
   Drivetrain.driveFor(forward, 39, inches, 20, velocityUnits::pct, true);
   Drivetrain.stop(hold);
   wait(500, msec);
-  Grabbers.stop();
   Drivetrain.driveFor(reverse, 5, inches, 50, velocityUnits::pct, true);
-  Drivetrain.turnFor(-120, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, true);
+  Drivetrain.turnFor(-150, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, true);
   wait(300, msec);
   Drivetrain.setTimeout(2, sec);
   Drivetrain.driveFor(forward, 25, inches, 33, velocityUnits::pct, true);
   Drivetrain.stop(hold);
+  Grabbers.stop();
   wait(300, msec);
 }
 
